64a60024a6d61498ec5baa85ce8546a869b54ace
awandiaye2403 <151854989+awandiaye2403@users.noreply.github.com>
2024-02-24 08:41:19 +0000

    Add files via upload

---
+++ Exojavascript/index.js
@@ -0,0 +1,13 @@
+var symptome=prompt("Quels symptome avez_vous?(abdomen,gorge");
+if (systome=== "abdomen"){
+alert("vous pourriez avoir une appendicite.");
+}else if (symptome=== "gorge"){
+    var symptome =prompt("Avez_vous de la fievre?(oui/nom)");
+    if (symptomegorge==="oui"){
+        alert("vous pourriez avoir un mal de gorge.");
+
+        
+    }
+}else{
+    alert("symptome non reconnu");
+}
